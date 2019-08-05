c165d062e1329ba58209b9c9594a618a7699595d
andresnew00 <andresnew00@hotmail.com>
2019-08-05 15:26:47 -0400

    passes all test homework number 1

---
+++ 20190806/src/main/java/com/company/MarvelTable.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class MarvelTable {
+
+    public static void main(String[] args) {
+
+        String table =
+                "-------------------------------------------------------------\n" +
+                "|   Hero              |   Affiliation     |   Power         |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Captain America   |   The Avengers    |   Peak Human    |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Wolverine         |   The X-Men       |   Healing/Claws |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Cyclops           |   The X-Men       |   Eye Beam      |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Domino            |   X-Force         |   Luck          |\n" +
+                "-------------------------------------------------------------";
+
+        System.out.println(table);
+
+    }
+}
