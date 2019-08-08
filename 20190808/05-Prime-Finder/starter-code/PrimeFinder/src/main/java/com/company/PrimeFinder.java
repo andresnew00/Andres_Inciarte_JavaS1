c3970a5c505e7ea344f67adb6393e6b67d2580e9
andresnew00 <andresnew00@hotmail.com>
2019-08-07 23:53:25 -0400

    parcial working code 20190808 part 2

--- 20190808/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
+++ 20190808/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PrimeFinder {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Please enter the limit number to find Prime");
+        int primeNumberLimit = Integer.parseInt(myScanner.nextLine());
+
+        for (int i = 1; i <= primeNumberLimit; i++) {
+
+
+
+
+
+            System.out.println(i);
+        }
 
     }
 }
--- 20190808/U1M1L3Katas/src/main/java/com/company/RangeChecker.java
+++ 20190808/U1M1L3Katas/src/main/java/com/company/RangeChecker.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RangeChecker {
 
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        //User Input
+        int userNumber = 0;
+
+        //User first try
+        System.out.println("Enter a number between 15 and 32");
+        userNumber = myScanner.nextInt();
+
+        while (userNumber < 15 || userNumber > 32) {
+
+            // Loop Try
+            System.out.println("Enter a number between 15 and 32");
+            userNumber = myScanner.nextInt();
+
+        }
+
+        System.out.println(userNumber);
 
     }
 }
