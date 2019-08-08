d19ed3c2e6e939f40940d2af6b4b15c75b53993f
andresnew00 <andresnew00@hotmail.com>
2019-08-08 02:50:24 -0400

    katas 3 complete commit with all tests passing

--- 20190808/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
+++ 20190808/05-Prime-Finder/starter-code/PrimeFinder/src/main/java/com/company/PrimeFinder.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 
 public class PrimeFinder {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        // User Input
+        System.out.println("Please enter a number: ");
 
-        System.out.println("Please enter the limit number to find Prime");
-        int primeNumberLimit = Integer.parseInt(myScanner.nextLine());
+        //User Choice for max number
+        int userNumber = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= primeNumberLimit; i++) {
+        for (int x = 2; x <= userNumber; x++) {
 
-
-
-
-
-            System.out.println(i);
+            boolean prime = true;
+            for (int i = 2; i <= x / 2; i++) {
+                if (x % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if ( prime == true)
+                System.out.println(x);
         }
-
     }
 }
