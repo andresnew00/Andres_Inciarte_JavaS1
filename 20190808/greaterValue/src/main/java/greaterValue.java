0f52b89ebdcfd080614589f5d638d33b210bd1d6
andresnew00 <andresnew00@hotmail.com>
2019-08-08 13:47:00 -0400

    rectpaving exercise fixed

---
+++ 20190808/greaterValue/src/main/java/greaterValue.java
@@ -0,0 +1,36 @@
+public class greaterValue {
+
+    //this code is not running but it should work, there is a line reading problem
+
+
+//    public static int sumInts(int num1, int num2) {
+//
+//        return num1 + num2;
+//
+//    }
+//    public static int multInts(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+//    public static void sayHi(){
+//        System.out.println("Hi!!!");
+//    }
+//
+//    public static void errorMessage(String msg) {
+//        System.out.println("An error has occurred " + msg);
+//    }
+//
+//    public static void main(String[] args) {​
+//        int sum = sumInts(3, 4);
+//        System.out.println(sum);
+//
+//        int product = multInts(2, 19);
+//        System.out.println(product);
+//
+//        sayHi();
+//
+//        errorMessage("I have nothing more to say ");
+//​
+//    }
+
+}
