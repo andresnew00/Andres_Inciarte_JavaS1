6e222b681cc6beebf16615ab86024ad570e559c0
andresnew00 <andresnew00@hotmail.com>
2019-08-09 14:02:36 -0400

    trial

---
+++ 20190808/greaterValue/src/main/java/Practice.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Practice {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int[] numbers = new int[10];
+//
+//        String stringIn;
+//
+//        int min;
+//        int max;
+//        int sum = 0;
+//        double avg;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println("Please enter a number: ");
+//            stringIn = scanner.nextLine();
+//            numbers[i] = Integer.parseInt(stringIn);
+//        }
+//
+//        // calculate max
+//
+//        max = numbers[0];
+//
+//        for(int i = 0; i < numbers.length; i++) {
+//            if(numbers[i] > max) {
+//                max = numbers[i];
+//            }
+//        }
+//
+//        //calculate min
+//
+//        min = numbers[0];
+//        for(int i = 0; i < numbers.length; i++) {
+//            if(numbers[i] < min) {
+//                min = numbers[i];
+//            }
+//        }
+//
+//        //calculate average
+//
+//        for(int i = 0; i < numbers.length; i++) {
+//            sum += numbers[i];
+//        }
+//
+//        avg = (double)sum/numbers.length;
+//
+//        System.out.println("Average is " + avg );
+//
+//        System.out.println("Minimum value is: " + min);
+//        System.out.println("Maximum value is: " + max);
+//        System.out.println("Sum: " + sum);
+//
+//
+//    }
+}
---
+++ 20190808/greaterValue/src/main/java/simpleClass.java
@@ -0,0 +1,38 @@
+public class simpleClass {
+
+//    public static void main(String[] args) {
+//        String aFruit = "Pineapple";
+//
+//        String[] fruits = {"apples", "oranges", "bananas"};
+//
+//        int[] numbers = {3, 9, 10, 11};
+//
+//        //------------------
+//
+//        int[] arr1 = new int[4];
+//        arr1[0] = 10;
+//        arr1[1] = 20;
+//        arr1[2] = 30;
+//        arr1[3] = 40;
+//
+//        System.out.println("the fourth house is = " + arr1[0]);
+//
+//        int[] arr2 = {10, 20, 30, 40};
+//
+//        arr2[3] = 100;
+//
+//        System.out.println("value at 4th location is : " + arr2[3]);
+//
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.println(arr2[i]);
+//        }
+//
+//        for (int element : arr2) {
+//            System.out.println(element);
+//        }
+//
+
+//    }
+
+
+}
